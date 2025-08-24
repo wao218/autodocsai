b492752ac936468748af41a09dc6f300a40b4370
wao218 <wesleyosborne1@gmail.com>
2025-08-24 13:45:16 -0400

    completed sidebar ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  images: {
+    remotePatterns: [new URL('https://avatar.iran.liara.run/*')],
+  },
 };
 
 export default nextConfig;
