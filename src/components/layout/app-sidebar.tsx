af02d9c1240e81228d2413c2d81bf80c1d0396de
Chad <chaddawick@gmail.com>
2025-08-25 14:49:48 -0500

    misspelled /dashboard

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -35,7 +35,7 @@ export default function AppSideBar() {
             <SidebarMenu>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
-                  <Link href='/dashbaord'>
+                  <Link href='/dashboard'>
                     <FileTextIcon />
                     <p className='text-base'>Documents</p>
                   </Link>
